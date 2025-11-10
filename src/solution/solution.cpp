5ba4bd0e9fc456f2e5dda4f542b5e16a2952bf73
AvadEu <dutwik44@wp.pl>
2025-11-10 18:34:29 +0100

    clean: remove unnecesary imports

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -1,16 +1,12 @@
 #include "tester.hpp"
 #include <CameraController.hpp>
-
-
-// You can remove or add any includes you need
 #include <chrono>
-#include <iostream>
 #include <thread>
 #include <memory>
 
 int solver(std::shared_ptr<backend_interface::Tester> tester, bool preempt) {
   const auto controller = std::make_unique<CameraController>(tester, preempt);
-  std::this_thread::sleep_for(std::chrono::seconds(10));
+  std::this_thread::sleep_for(std::chrono::seconds(30));
 
   return 0;
 }
